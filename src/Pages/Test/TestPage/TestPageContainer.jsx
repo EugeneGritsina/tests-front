a0fb6e63691d8a924dc3f3579ec3f56173c818f0
Eugene Gritsina <eugene.gritsina@gmail.com>
2021-05-09 22:14:30 +0300

    feature: adding image to test page background

--- src/Pages/Test/TestPage/TestPageContainer.jsx
+++ src/Pages/Test/TestPage/TestPageContainer.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { Button, Typography, Divider, Paper, Grid } from '@material-ui/core';
 
+import '../../../index.css';
 import Loader from '../../../Components/Loader/Loader.jsx';
 import * as Actions from './TestActions.js';
 import * as Selectors from './TestSelectors.js';
@@ -25,40 +26,42 @@ class TestPageContainer extends Component {
         if(!test) return(<Loader />);
 
         return (
-            <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justify="center"
-            style={{ minHeight: '10vh' }}
-            >
-                <Button onClick={history.goBack}>Назад</Button>
-                <Paper elevation={3}>
-                    <h1>Название теста: {test.name}</h1>
-                    <Divider />
-                    <Typography>Предмет: {test.subjectDTO.name}</Typography>
-                    <Divider /> 
-                    <Typography>Количество вопросов, отображаемое студенту: {test.questionsAmount}</Typography>
-                    <Divider /> 
-                    <Typography>Время прохождения теста: {test.estimatedTime ? test.estimatedTime : "Не задано"}</Typography>
-                    <Divider /> 
-                    <Typography>Доступ к тесту: {test.isOpen ? "Открыт" : "Закрыт"}</Typography>
-                    <Divider /> 
-                </Paper>
-                {test.questions.map(question => {
-                    return (
-                        <Paper elevation={3} key={question.id}>
-                            <h2>Вопрос: {question.description}</h2>
-                            <Divider /> 
-                            <Typography>Тип вопроса: {question.questionType.type}</Typography>
-                            <Divider /> 
-                            <Typography>Количество баллов: {question.points}</Typography>
-                            <Divider /> 
-                        </Paper>
-                    )
-                })}
-            </Grid>
+            <div className='page-wrapper'>
+                <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justify="center"
+                style={{ minHeight: '10vh' }}
+                >
+                    <Button onClick={history.goBack}>Назад</Button>
+                    <Paper elevation={3}>
+                        <h1>Название теста: {test.name}</h1>
+                        <Divider />
+                        <Typography>Предмет: {test.subjectDTO.name}</Typography>
+                        <Divider /> 
+                        <Typography>Количество вопросов, отображаемое студенту: {test.questionsAmount}</Typography>
+                        <Divider /> 
+                        <Typography>Время прохождения теста: {test.estimatedTime ? test.estimatedTime : "Не задано"}</Typography>
+                        <Divider /> 
+                        <Typography>Доступ к тесту: {test.isOpen ? "Открыт" : "Закрыт"}</Typography>
+                        <Divider /> 
+                    </Paper>
+                    {test.questions.map(question => {
+                        return (
+                            <Paper elevation={3} key={question.id}>
+                                <h2>Вопрос: {question.description}</h2>
+                                <Divider /> 
+                                <Typography>Тип вопроса: {question.questionType.type}</Typography>
+                                <Divider /> 
+                                <Typography>Количество баллов: {question.points}</Typography>
+                                <Divider /> 
+                            </Paper>
+                        )
+                    })}
+                </Grid>
+            </div>
         );
     }
 }
